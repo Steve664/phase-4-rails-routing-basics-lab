d804fdb3da07bfcdc034442606b10c409354192c
Steve Kariuki <stevekariuki5@gmail.com>
2023-03-19 11:43:44 +0300

    Completed

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+    def index 
+        students = Student.all
+        render json: students
+    end
+    def grades
+        students = Student.order('grade DESC')
+        render json: students
+    end
+    def highest_grade
+        students = Student.order('grade DESC').limit(1)
+        render json: students
+    end
+end
